ca6c77e57f7fc0f374c3ccfc5c668e3165f432d3
Sefa Gueven <sefagueven@gmx.de>
2026-05-15 14:24:07 +0200

    Questions wrapped in a JsonArray.

---
+++ script.js
@@ -0,0 +1,18 @@
+let questions = [
+    {
+        "question": "Wofür wird CSS verwendet?",
+        "answer_1": "Für Logik im Code",
+        "answer_2": "Für Datenbanken",
+        "answer_3": "Für das Design und Layout von Webseiten",
+        "answer_4": " Für Server-Verbindungen",
+        "right_answer": 3
+    },
+    {
+        "question": "Was braucht man, um eine Website im Internet anzusehen?",
+        "answer_1": "Einen Drucker",
+        "answer_2": "Einen Webbrowser",
+        "answer_3": "Eine Tastatur",
+        "answer_4": "Eine Kamera",
+        "right_answer": 2
+    }
+]
