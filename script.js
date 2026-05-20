a24d5d0ce358a69600b528efbf829723d6086306
Sefa Gueven <sefagueven@gmx.de>
2026-05-20 12:08:19 +0200

    Display answers in color: green for correct answers, red for incorrect answers

--- script.js
+++ script.js
@@ -5,18 +5,30 @@ function init() {
     showQuestion();
 }
 
-function showQuestion(){
-    let question =questions[currentQuestion];
+function showQuestion() {
+    let question = questions[currentQuestion];
 
     document.getElementById('questiontext').innerHTML = question['question'];
-     document.getElementById('answer_1').innerHTML = question['answer_1'];
-      document.getElementById('answer_2').innerHTML = question['answer_2'];
-       document.getElementById('answer_3').innerHTML = question['answer_3'];
-        document.getElementById('answer_4').innerHTML = question['answer_4'];
+    document.getElementById('answer_1').innerHTML = question['answer_1'];
+    document.getElementById('answer_2').innerHTML = question['answer_2'];
+    document.getElementById('answer_3').innerHTML = question['answer_3'];
+    document.getElementById('answer_4').innerHTML = question['answer_4'];
 
 }
-function answer(selection){
-    console.log('selected answer is',selection);
-    
+function answer(selection) {
+    let question = questions[currentQuestion];
+    console.log('selected answer is', selection);
+    let selectedQuestionNumber = selection.slice(-1);               //slice(-1)=> Gibt das letzte Element – egal ob Array oder String.
+    console.log(selectedQuestionNumber);
+    console.log('Current question is ', question['right_answer']);
 
+    let idOfRightAnswer = `answer_${question['right_answer']}`;     //Diese Zeile baut dynamisch eine ID zusammen, um die richtige Antwort im HTML zu finden. Das ist ein Template String (mit ` statt " ") Vorteil: Du kannst Variablen direkt einbauen (${...})
+
+    if (selectedQuestionNumber == question['right_answer']) {       //Ist die ausgewählte Antwort gleich der richtigen Antwort?
+        console.log('Richtige Antwort');                            //Wird ausgeführt, wenn die Antwort stimmt.
+        document.getElementById(selection).parentNode.classList.add('bg-success'); // mit dem befehl"parentNode" zu Eltern-Element gehen.                   
+    }
+    else                               // Wird ausgeführt, wenn die Antwort falsch ist.
+        document.getElementById(selection).parentNode.classList.add('bg-danger');  
+        document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');    // richtige wird dan hier auch angezeigt. 
 }
